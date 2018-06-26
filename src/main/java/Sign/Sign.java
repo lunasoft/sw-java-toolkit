e756d896e0c6ab4178dc71928aef66e1f1919d6d
Emmanuel <emmanuel.contreras@sw.com.mx>
2018-06-26 14:39:49 -0500

    Cambios en la libreria de sellado, generación key base64, cadena original y pruebas unitarias

--- src/main/java/Sign/Sign.java
+++ src/main/java/Sign/Sign.java
@@ -18,27 +18,32 @@ import java.security.cert.X509Certificate;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import com.sun.org.apache.xerces.internal.impl.dv.util.Base64;
+import java.nio.charset.StandardCharsets;
+import java.security.KeyFactory;
+import java.security.PrivateKey;
+import java.security.spec.PKCS8EncodedKeySpec;
 import javax.xml.bind.DatatypeConverter;
 
 import static javax.xml.bind.DatatypeConverter.parseBase64Binary;
 import static javax.xml.bind.DatatypeConverter.printBase64Binary;
 
 public class Sign {
-
+      
+     
     public static String signGet(String cadenaOriginal, String KeyBase64, String passwordLlave) throws GeneralSecurityException, IOException {
-        InputStream myInputStream = new ByteArrayInputStream(parseBase64Binary(KeyBase64));
-        PKCS8Key pkcs8 = new PKCS8Key(myInputStream, passwordLlave.toCharArray());
+        PKCS8Key pkcs8 = new PKCS8Key(java.util.Base64.getDecoder().decode(KeyBase64), passwordLlave.toCharArray());
         java.security.PrivateKey pk = pkcs8.getPrivateKey();
         Signature signature = Signature.getInstance("SHA256withRSA");
         signature.initSign(pk);
         signature.update(cadenaOriginal.getBytes("UTF-8"));
-        return printBase64Binary(signature.sign());
+        return printBase64Binary(signature.sign());      
     }
 
-    public String originalStringGet(String xml) {
+    public static String originalStringGet(String xml) {
         try {
             TransformerFactory transformerFactory = TransformerFactory.newInstance();
-            Source xslt = new StreamSource(new File("src/test/java/Tests/assets/cadenaoriginal_3_3.xslt"));
+            String filePath = new File("").getAbsolutePath();
+            Source xslt = new StreamSource(new File(filePath.concat("/src/main/Resources/assets/cadenaoriginal_3_3.xslt")));
             Transformer transformer = transformerFactory.newTransformer(xslt);
             Source xmlSource = new StreamSource(new StringReader(xml));
             ByteArrayOutputStream baos = new ByteArrayOutputStream();
@@ -48,7 +53,6 @@ public class Sign {
             String cadOrig = new String(cadenaOriginalArray, "UTF-8");
             return cadOrig;
         } catch (TransformerException | UnsupportedEncodingException e) {
-            e.printStackTrace();
         }
         return null;
     }
@@ -64,8 +68,15 @@ public class Sign {
       return Base64.encode(cert.getEncoded());
     }
     
-    public static String keyBase64get(final String key) throws IOException {
-        return DatatypeConverter.printBase64Binary(new String(Files.readAllBytes( Paths.get(key))).getBytes());
+    public static String keyBase64get(final String path) throws FileNotFoundException, IOException{
+        File file = new File(path);
+        byte[] fileBytes;
+        try (FileInputStream fileInputStream = new FileInputStream(file)) {
+            fileBytes = new byte[fileInputStream.available()];
+            fileInputStream.read(fileBytes);
+        }
+        String fileString = new String(java.util.Base64.getEncoder().encode(fileBytes));
+        return fileString;
     }
 
     public static X509Certificate attributesCertificateGet(final X509Certificate cert){
